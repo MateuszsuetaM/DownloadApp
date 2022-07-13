e2bcbc6911ab94bfa6c352840e1d6212d2bcff97
Mateusz <mzarzycki1212@gmail.com>
2022-07-13 23:22:20 +0200

    ADD - dodanie Pobierania plików

---
+++ Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using DownloadApp.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DownloadApp.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class FileController : Controller
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("ShowFiles")]
+        public IActionResult Index()
+        {
+            string[] filePaths = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "Files/"));
+
+            List<FileModel> files = new List<FileModel>();
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new FileModel { FileName = Path.GetFileName(filePath) });
+            }
+
+            return View(files);
+        }
+
+        [Authorize]
+        public FileResult DownloadFile(string fileName)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Files/") + fileName;
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, "application/octet-stream", fileName);
+        }
+    }
+}
---
+++ DTO/FileModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DownloadApp.DTO
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+    }
+}
