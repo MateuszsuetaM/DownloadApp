7ce865a140ef1bd1d3dc38b8769c2519c28fa1ef
Mateusz <mzarzycki1212@gmail.com>
2022-07-13 17:01:10 +0200

    ADD - dodanie kontrolera

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DownloadApp.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Registration")]
+        public string get()
+        {
+            return "test";
+        }
+    }
+}
